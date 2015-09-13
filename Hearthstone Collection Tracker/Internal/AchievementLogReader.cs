f52e2250cdc10c6977916f091b5b8bf704bb3db2
riQQ <riQQ@users.noreply.github.com>
2015-09-13 10:49:25 +0200

    automatically update collection by parsing Achievements.log

---
+++ Hearthstone Collection Tracker/Internal/AchievementLogReader.cs
@@ -0,0 +1,18 @@
+﻿using Hearthstone_Deck_Tracker.LogReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearthstone_Collection_Tracker.Internal
+{
+    public class AchievementLogReader : LogReaderInfo
+    {
+        public AchievementLogReader()
+        {
+            this.Name = "Achievements";
+            this.ContainsFilters = new[] { "NotifyOfCardGained" };
+        }
+    }
+}
