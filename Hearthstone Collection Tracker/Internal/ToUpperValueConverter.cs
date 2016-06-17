ebabe431667ba0f81ca1f88de2e9074f763d828a
Konstantin Vasilev <oppa.kostya.bko@gmail.com>
2016-06-17 23:09:16 +0700

    Allow to collapse set blocks.

---
+++ Hearthstone Collection Tracker/Internal/ToUpperValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Hearthstone_Collection_Tracker.Internal
+{
+    public class ToUpperValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                              CultureInfo culture)
+        {
+            if (value is string)
+            {
+                return value.ToString().ToUpper();
+            }
+            return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
