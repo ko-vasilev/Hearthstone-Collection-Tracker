906c3a6bff4ddad36b9a8f8db495a51fab0ffd31
Konstantin Vasilev <oppa.kostya.bko@gmail.com>
2016-06-22 20:15:19 +0700

    Update plugin version.

---
+++ Hearthstone Collection Tracker/Internal/DataUpdaters/DataUpdaterV05.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hearthstone_Collection_Tracker.Internal.DataUpdaters
+{
+    public class DataUpdaterV05 : BaseUpdaterByVersion
+    {
+        private static readonly Version _version = new Version(0, 5, 0);
+
+        public override Version Version
+        {
+            get
+            {
+                return _version;
+            }
+        }
+    }
+}
--- Hearthstone Collection Tracker/Internal/DataUpdaters/DefaultDataUpdater.cs
+++ Hearthstone Collection Tracker/Internal/DataUpdaters/DefaultDataUpdater.cs
@@ -14,7 +14,8 @@ namespace Hearthstone_Collection_Tracker.Internal.DataUpdaters
             new DataUpdaterV031(),
             new DataUpdaterV032(),
             new DataUpdaterV04(),
-            new DataUpdaterV041()
+            new DataUpdaterV041(),
+            new DataUpdaterV05()
         };
 
         /// <summary>
