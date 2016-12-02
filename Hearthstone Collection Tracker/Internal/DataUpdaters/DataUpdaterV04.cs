f272f3b595b1e971b9974fa5da72cccf0276e91e
Andrew Pang <pang.134@osu.edu>
2016-12-02 01:31:03 -0500

    Update DataUpdaterV04.cs

--- Hearthstone Collection Tracker/Internal/DataUpdaters/DataUpdaterV04.cs
+++ Hearthstone Collection Tracker/Internal/DataUpdaters/DataUpdaterV04.cs
@@ -39,7 +39,7 @@ namespace Hearthstone_Collection_Tracker.Internal.DataUpdaters
                     var settings = Hearthstone_Deck_Tracker.XmlManager<PluginSettings>.Load(configFilePath);
                     return settings.CurrentVersion < new ModuleVersion(_version);
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     return false;
                 }
